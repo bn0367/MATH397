f932ddef9382f354b46ac6b1fc3615bb5194ea77
bn0367 <bnewman@oberlin.edu>
2022-02-28 12:03:24 -0500

    generalized nbodyproblem properly

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 - [coolpattern1.eps](coolpattern1.eps)
 - [coolrainbow.eps](coolrainbow.eps)
 - [image.eps](image.eps)
-- 3 Body Problem!  
+- N Body Problem!  
 -  ![3 Body Problem](nbodyproblem/output.gif)
 
 ### WIP:
@@ -20,3 +20,4 @@
 - 2022-02-25: Finished image/image2
 - 2022-02-26: Started work on nbodyproblem
 - 2022-02-27: Finished nbodyproblem, started on orbit + epicycloid orbit
+- 2022-02-28: Generalized nbodyproblem to actually be n bodies and not 3
--- nbodyproblem/nbodyproblem.py
+++ nbodyproblem/nbodyproblem.py
@@ -5,66 +5,57 @@ from tqdm import tqdm  # progress bar
 import os
 
 # algorithm adapted from https://blbadger.github.io/3-body-problem.html
-# changed to be 2d
+# changed to be 2d, extended to be n-bodies
 
 iterations = 200000
-delta = 0.0005
+delta = 0.0001
 G = 9.8
 
+colors = [(1.0, 0.0, 0.0), (0.0, 1.0, 0.0), (0.0, 0.0, 1.0), (1.0, 0.0, 1.0)]
+
 # masses
-m1, m2, m3 = 10, 20, 30
+masses = [10, 20, 30, 40]
 
 # start positions
-p1_start, p2_start, p3_start = np.array(
+position_starts = [np.array(
     [10., 10.]), np.array(
     [0., 0.]), np.array(
-    [10., -10.])
+    [10., -10.]), np.array([-10., 10.])]
 
 # start velocities
-v1_start, v2_start, v3_start = np.array(
+velocity_starts = [np.array(
     [0., 0.]), np.array(
     [0., 0.]), np.array(
-    [0., 0.])
+    [0., 0.]), np.array([0., 0.])]
 
 
 # calculates derivatives (accelerations) of the 3 bodies
-def accelerations(p1, p2, p3, m1, m2, m3):
-    planet_1_dv = -G * m2 * (p1 - p2) / (
-            np.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2) ** 2) - \
-                  G * m3 * (p1 - p3) / (
-                          np.sqrt((p1[0] - p3[0]) ** 2 + (p1[1] - p3[1]) ** 2) ** 2)
-
-    planet_2_dv = -G * m3 * (p2 - p3) / (
-            np.sqrt((p2[0] - p3[0]) ** 2 + (p2[1] - p3[1]) ** 2) ** 2) - \
-                  G * m1 * (p2 - p1) / (
-                          np.sqrt((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2) ** 2)
-
-    planet_3_dv = -G * m1 * (p3 - p1) / (
-            np.sqrt((p3[0] - p1[0]) ** 2 + (p3[1] - p1[1]) ** 2) ** 2) - \
-                  G * m2 * (p3 - p2) / (
-                          np.sqrt((p3[0] - p2[0]) ** 2 + (p3[1] - p2[1]) ** 2) ** 2)
-
-    return planet_1_dv, planet_2_dv, planet_3_dv
+def accelerations(p, m):
+    acc = []
+    for i in range(len(p)):
+        dv = sum(-G * m[x] * (p[i] - p[x])
+                 / (np.sqrt((p[i][0] - p[x][0]) ** 2 + (p[i][1] - p[x][1]) ** 2) ** 2) for x in range(len(p)) if x != i)
+        acc.append(dv)
+    return acc
 
 
 # create arrays for the positions and velocities
-p1, p2, p3 = [[[0., 0.] for _ in range(iterations)] for _ in range(3)]
-v1, v2, v3 = [[[0., 0.] for _ in range(iterations)] for _ in range(3)]
+positions = [[np.array([0., 0.]) for _ in range(iterations)] for _ in range(len(masses))]
+velocities = [[np.array([0., 0.]) for _ in range(iterations)] for _ in range(len(masses))]
 
 # set initial values
-p1[0], p2[0], p3[0] = p1_start, p2_start, p3_start
-v1[0], v2[0], v3[0] = v1_start, v2_start, v3_start
+for i in range(len(masses)):
+    positions[i][0] = position_starts[i]
+    velocities[i][0] = velocity_starts[i]
 
 for i in tqdm(range(iterations - 1)):
-    dv1, dv2, dv3 = accelerations(p1[i], p2[i], p3[i], m1, m2, m3)
+    dvs = accelerations([x[i] for x in positions], masses)
 
-    v1[i + 1] = v1[i] + dv1 * delta
-    v2[i + 1] = v2[i] + dv2 * delta
-    v3[i + 1] = v3[i] + dv3 * delta
+    for m in range(len(masses)):
+        velocities[m][i + 1] = velocities[m][i] + dvs[m] * delta
 
-    p1[i + 1] = p1[i] + v1[i] * delta
-    p2[i + 1] = p2[i] + v2[i] * delta
-    p3[i + 1] = p3[i] + v3[i] * delta
+    for m in range(len(masses)):
+        positions[m][i + 1] = positions[m][i] + velocities[m][i] * delta
 
     if (i - 500) % 1000 == 0 and i > 0:
         eps = Eps("nbodyproblem%d" % ((i - 500) / 1000), 500)
@@ -79,23 +70,12 @@ for i in tqdm(range(iterations - 1)):
 
         # use old points to make frame transition smoother
         for k in range(i - 500, i + 499):
-            if all(p1[k]) and all(p1[k + 1]):
-                eps.add("%f %f moveto" % (p1[k][0], p1[k][1]))
-                eps.add("%f %f lineto" % (p1[k + 1][0], p1[k + 1][1]))
-                eps.add("%f %f %f setrgbcolor" % (1, 0, 0))
-                eps.add("stroke")
-
-            if all(p2[k]) and all(p2[k + 1]):
-                eps.add("%f %f moveto" % (p2[k][0], p2[k][1]))
-                eps.add("%f %f lineto" % (p2[k + 1][0], p2[k + 1][1]))
-                eps.add("%f %f %f setrgbcolor" % (0, 1, 0))
-                eps.add("stroke")
-
-            if all(p3[k]) and all(p3[k + 1]):
-                eps.add("%f %f moveto" % (p3[k][0], p3[k][1]))
-                eps.add("%f %f lineto" % (p3[k + 1][0], p3[k + 1][1]))
-                eps.add("%f %f %f setrgbcolor" % (0, 0, 1))
-                eps.add("stroke")
+            for j in range(len(masses)):
+                if all(positions[j][k]) and all(positions[j][k + 1]):
+                    eps.add("%f %f moveto" % (positions[j][k][0], positions[j][k][1]))
+                    eps.add("%f %f lineto" % (positions[j][k + 1][0], positions[j][k + 1][1]))
+                    eps.add("{} {} {} setrgbcolor".format(*colors[j]))
+                    eps.add("stroke")
 
         eps.add("closepath")
         eps.add("showpage")
