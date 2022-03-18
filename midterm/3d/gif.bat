262450a40eae8905eea7c567eeed2c2123ffac95
bn0367 <bnewman@oberlin.edu>
2022-03-18 15:41:15 -0400

    new gifs

--- midterm/3d/gif.bat
+++ midterm/3d/gif.bat
@@ -1,3 +1,3 @@
-gswin64c -q -dBATCH -dNOSAFER -dNOPAUSE -sDEVICE=png256 -sOutputFile=frames/frame%%ld.png rotating_square.eps
-ffmpeg -f image2 -framerate 15 -i frames/frame%%d.png -y gol.gif
+gswin64c -dBATCH -dNOSAFER -dNOPAUSE -sDEVICE=png256 -sOutputFile=frames/frame%%ld.png rotating_square.eps
+ffmpeg -f image2 -framerate 15 -i frames/frame%%d.png -y gol_glider_gun.gif
 del "frames\*.png"
