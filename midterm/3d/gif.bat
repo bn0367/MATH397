e885246a4c06334e1c17a7a00dc6df84245b0650
bn0367 <bnewman@oberlin.edu>
2022-04-01 00:05:19 -0400

    maybe more like opengl soon, paper work, cool gif :tm:

--- midterm/3d/gif.bat
+++ midterm/3d/gif.bat
@@ -1,3 +1,3 @@
-gswin64c -dBATCH -dNOSAFER -dNOPAUSE -sDEVICE=png256 -sOutputFile=frames/frame%%ld.png rotating_square.eps
-ffmpeg -f image2 -framerate 15 -i frames/frame%%d.png -y gol_glider_gun.gif
+gswin64c -dBATCH -dNOSAFER -dNOPAUSE -sDEVICE=png256 -sOutputFile=frames/frame%%ld.png -g500x500 test_old.eps
+ffmpeg -f image2 -framerate 30 -i frames/frame%%d.png -y paper1.gif
 del "frames\*.png"
